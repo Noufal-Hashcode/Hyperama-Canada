5cc4f0a222650c882e9bd66ce738f05960ed7be8
Mohammed Nabeel <nabeel@hashcodeit.com>
2023-12-18 10:27:55 +0400

    Barcode Check sum module update

--- hc_add_barcode_check_sum/__manifest__.py
+++ hc_add_barcode_check_sum/__manifest__.py
@@ -3,16 +3,14 @@
     'author': "nbl",
     'maintainer': 'nbl',
     'website': 'https://hashcodeit.com/',
-    'version': '17.0.1.0',
+    'version': '17.0.1.1',
     'support': 'info@hashcodeit.com',
     'category': '',
     'summary': 'Add EAN13 Barcode Check Sum',
     'description': """ """,
     'depends': ['stock', 'product'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/res_partner_view.xml',
-        'views/customer_type_view.xml'
+        'views/product_product.xml',
     ],
     "images": ["static/description/icon.png"],
     'license': 'OPL-1',
@@ -21,21 +19,3 @@
     'application': False,
 }
 
-
-{
-    'name': 'Add EAN13 Barcode Check Sum',
-    "author": "",
-    'version': '17.0.0.0',
-    'category': 'stock',
-    'sequence': 6,
-    'summary': 'Add EAN13 Barcode Check Sum',
-    'description': """""",
-    'depends': ['stock', 'product'],
-    'data': [
-        'views/product_product.xml',
-    ],
-    'assets': {},
-    'installable': True,
-    'auto_install': True,
-    'license': 'LGPL-3',
-}
