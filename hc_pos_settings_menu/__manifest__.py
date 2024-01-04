a2340525c00aaa7e02390f61d82cbad789467e76
Mohammed Nabeel <nabeel@hashcodeit.com>
2024-01-04 10:35:56 +0400

    Add pos settings menu module

---
+++ hc_pos_settings_menu/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'POS Settings Menu',
+    "author": "",
+    'version': '17.0.0.0',
+    'category': 'point_of_sale',
+    'sequence': 6,
+    'summary': 'POS Settings Menu',
+    'description': """
+.
+""",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/point_of_sale_view.xml'
+    ],
+    'assets': {  },
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
