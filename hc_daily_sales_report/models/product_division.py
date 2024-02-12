36c907219aca69bff9837baa88d1e92c6fa085a9
Firasa-Hashcode <Firasa@hashcodeit.com>
2024-02-12 10:33:04 +0400

    daily sales report new Module

---
+++ hc_daily_sales_report/models/product_division.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+import base64
+
+
+class ProductDivision(models.Model):
+    _name = 'product.division'
+
+    name = fields.Char("Name")
+
+    # @api.model
+    # def send_email_with_attachment(self):
+    #     wizard = self.env['daily.sales.report.wizard'].create({})
+    #     report_content = wizard.generate_daily_report()
+    #     recipients = wizard.get_email_recipients()
+    #     if report_content and recipients:
+    #         print("Sending report to:", recipients)
+    #         print("Report content:", report_content)
+    #         wizard.send_daily_report_email(report_content, recipients)
