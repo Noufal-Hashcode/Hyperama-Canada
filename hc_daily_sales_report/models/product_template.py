e61c0064459825f62c3c720481f9e094fc0d1fa2
Firasa-Hashcode <Firasa@hashcodeit.com>
2024-03-19 09:04:02 +0400

    server action

--- hc_daily_sales_report/models/product_template.py
+++ hc_daily_sales_report/models/product_template.py
@@ -5,3 +5,10 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     division = fields.Many2one('product.division', 'Division')
+
+    def update_product_division(self):
+        for rec in self:
+            if not rec.division and rec.product_tag_ids:
+                division = self.env['product.division'].search([('name', '=', rec.product_tag_ids[0].name)], limit=1)
+                if division:
+                    rec.division = division.id
